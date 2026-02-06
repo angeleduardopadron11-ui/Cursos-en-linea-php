a8908311cdfef55b91198a128aa6c661f39aa95c
tu pagina web <230896647+angeleduardopadron11-ui@users.noreply.github.com>
2026-02-05 22:53:29 -0400

    Delete tienda directory

--- tienda/app/Controllers/DashboardController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Controllers;
-
-class DashboardController {
-    public function index() {
-        // La seguridad ya la manejó el index.php
-        require_once '../app/views/dashboard/index.php';
-    }
-}
